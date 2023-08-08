0ebd75540e00371c1c609a28a7d3eb6f685e395d
Niklas Kiefer <niklas.kiefer@camunda.com>
2023-08-08 16:04:54 +0200

    feat: bootstrap next app

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Custom Form Field Editor',
+  description: 'Edit custom form fields for Camunda Forms',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,48 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+import Btn from './Btn'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center pb-6 pt-8 lg:static lg:w-auto">
+          Get started by selecting an extension
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/next.svg"
+              alt="Next Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+      <div className="max-w-5xl w-full justify-between flex">
+        <div className="left-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 lg:gap-8">
+          <Btn title="Number range" />
+          <Btn title="foo" />
+          <Btn title="bar" />
+          <Btn title="xxx" />
+        </div>
+      </div>
+      <div className="max-w-5xl flex flex-col w-full">
+        <p className="fixed left-0 top-0 flex w-full pb-6 pt-8 lg:static lg:w-auto">
+          Or open the preview directly
+        </p>
+        <Link href="/preview"><Btn title='Open preview'></Btn></Link>
+      </div>
+    </main>
+  )
+}
